57c81d98405de14038eccc3ebf4f799b5ea0c24b
cmbreki <cbreki@hotmail.gr>
2016-08-10 00:22:10 +0300

    Fix query in Show controller

--- public/js/controllers/noteShowController.js
+++ public/js/controllers/noteShowController.js
@@ -1,10 +1,14 @@
-angular.module('noteApp').controller('NoteShowController',function(){
+angular.module('noteApp').controller('NoteShowController',['Note','$scope','$http','$routeParams',function(Note,$scope,$http,$routeParams){
+
+ $scope.note = Note.query({id:$routeParams.id});
+  console.log($scope.note);
+  
+
+/* $http.get('/notes/'+$routeParams.id).success(function(response){
+
+   console.log(response);
+
+ }); */
 
 
-
-
-
-
-
-
-});
+}]);
