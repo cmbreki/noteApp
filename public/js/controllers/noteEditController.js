81b8a66681e075e421ba61e1b6aebf28d80e9355
cmbreki <cbreki@hotmail.gr>
2016-08-14 19:00:37 +0300

    Fix update functionality

--- public/js/controllers/noteEditController.js
+++ public/js/controllers/noteEditController.js
@@ -1,11 +1,17 @@
 angular.module('noteApp').controller('NoteEditController',['Note','$scope','$http','$routeParams',function(Note,$scope,$http,$routeParams){
 
-$scope.note = Note.query({id:$routeParams.id});
+ $scope.note = Note.query({id:$routeParams.id});
+ $scope.isSubmitting = false;
+ console.log($scope.note);
 
 
 $scope.saveNote = function(note){
-  note.$update();
-  //console.log(note);
+    $scope.isSubmitting=true;
+    note.$update().finally(function(){
+      $scope.isSubmitting=false;
+      console.log($scope.note);
+  });
+  //
 
 }
 
