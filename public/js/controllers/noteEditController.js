2f601aaf49851936dc51b49e9aff825470e5d9d2
cmbreki <cbreki@hotmail.gr>
2016-08-14 17:15:52 +0300

    Add update function

--- public/js/controllers/noteEditController.js
+++ public/js/controllers/noteEditController.js
@@ -1,12 +1,11 @@
 angular.module('noteApp').controller('NoteEditController',['Note','$scope','$http','$routeParams',function(Note,$scope,$http,$routeParams){
 
 $scope.note = Note.query({id:$routeParams.id});
-console.log($scope.note);
+
 
 $scope.saveNote = function(note){
-  //note.$update();
-
-  console.log(note);
+  note.$update();
+  //console.log(note);
 
 }
 
--- public/js/services/note.js
+++ public/js/services/note.js
@@ -1,4 +1,12 @@
 angular.module('noteApp').factory('Note',function($resource){
-  return $resource('/notes/:id');
+  return $resource('/notes/:id', {id:"@id"},{
+
+      //custom update method
+      update:{
+        method:"PUT"        
+      }
+
+
+  } );
 
 });
