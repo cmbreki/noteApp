ace0093c4120f0c8df4ddc7b10633a537fe6f104
cmbreki <cbreki@hotmail.gr>
2016-08-25 02:36:29 +0300

    Add new note

--- public/js/controllers/noteCreateController.js
+++ public/js/controllers/noteCreateController.js
@@ -1,10 +1,18 @@
-angular.module('noteApp').controller('NoteCreateController',function(){
+angular.module('noteApp').controller('NoteCreateController',['$scope','Note','$location',function($scope,Note,$location){
+  $scope.note = new Note();
+  $scope.isSubmitting = false;
 
+ $scope.saveNote = function(note){
 
+  $scope.note.category='General';
+  $scope.note.category_id = 1;
+  //$scope.note.date = Date.now();
+  $scope.note.date = "24-12-2016";
+  $scope.isSubmitting=true;
+  note.$save().finally(function(){
+      $scope.isSubmitting = false;
+      $location.path('/notes/');
+  });
 
-
-
-
-
-
-});
+ }
+}]);
